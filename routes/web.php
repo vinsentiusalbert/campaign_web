dca202d10b5c62d541687e90f7e632353b3e601b
albert <vinsentiusalbert@gmail.com>
2026-01-26 14:22:34 +0700

    fix
    fix

--- routes/web.php
+++ routes/web.php
@@ -9,8 +9,8 @@ use App\Http\Controllers\CampaignMobileController;
 use App\Http\Controllers\CampaignIndihomeController;
 
 Route::get('/', [FrontController::class, 'index'])->name('home');
-Route::get('/login', [FrontController::class, 'index'])->name('login');
-Route::post('/login', [BackController::class, 'login'])->name('login.post');
+Route::get('/login', [FrontController::class, 'index']);
+Route::post('/login', [BackController::class, 'login'])->name('login');
 Route::get('/register', [FrontController::class, 'register'])->name('register');
 Route::post('/register-simpan', [BackController::class, 'registerStore'])->name('register.store');
 
